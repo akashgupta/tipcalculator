57bd240aff337ac700ed0eceac3479bbabe40901
Akash Gupta <akash@fb.com>
2014-10-10 01:05:43 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Akash Gupta on 10/10/14.
+//  Copyright (c) 2014 Akash Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipcalculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Akash Gupta on 10/10/14.
+//  Copyright (c) 2014 Akash Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
